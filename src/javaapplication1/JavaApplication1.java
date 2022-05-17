0b453fdd64012048694a1459db8ac12b89df79e5
Fajdetia <105681755+Fajdetia@users.noreply.github.com>
2022-05-16 20:05:00 -0400

    commit

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,32 @@
+package javaapplication1;
+import java.util.Scanner;
+/**
+ *
+ * @author Owner
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Enter your string here: ");
+        String s=scan.nextLine();
+        
+        char[] revString=new char[s.length()];
+        
+        for(int i=0; i<s.length(); i++)
+        {
+            revString[i]=s.charAt(i);
+        }
+        
+        for(int i=revString.length-1; i>=0;i--)
+        {
+            System.out.print(revString[i]);
+        }
+        
+        }
+    }
+    
+
